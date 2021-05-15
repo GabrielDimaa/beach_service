b5e7ce172cb270389c210e86dfc6d87f6d730692
Gabriel de Matos Hainzenreder <44183128+GabrielDimaa@users.noreply.github.com>
2021-05-15 17:00:00 -0300

    Removendo comentário

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,3 @@ import 'app/app_module.dart';
 import 'app/app_widget.dart';
 
 void main() => runApp(ModularApp(module: AppModule(), child: AppWidget()));
-
-// SQFLiteHelper sql = SQFLiteHelper();
-// sql.getDb();
-// final path = await sql.getPath();
-// final origFile = File(path);
-// var destFileName = 'beach_service_db.db';
-// final tempDir = await getTemporaryDirectory();
-// final tempFile = File('${tempDir.path}/$destFileName');
-// tempFile.writeAsBytesSync(origFile.readAsBytesSync());
-// ShareExtend.share(tempFile.path, "file");
