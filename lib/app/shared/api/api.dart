a83234a73e1fdca806b2aba344fb0ae7c0cc4be7
Gabriel de Matos Hainzenreder <44183128+GabrielDimaa@users.noreply.github.com>
2021-07-02 21:37:37 -0300

    Alteração na logo para geração da realease

--- lib/app/shared/api/api.dart
+++ lib/app/shared/api/api.dart
@@ -2,7 +2,7 @@ import 'package:beach_service/app/shared/api/interceptors_api.dart';
 import 'package:dio/native_imp.dart';
 
 abstract class BaseURL {
-  static String baseURL = "http://4c909aa559ab.ngrok.io";
+  static String baseURL = "http://5c9485066c0e.ngrok.io";
 }
 
 class Api extends DioForNative {
